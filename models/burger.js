0c8cf194b3d8b464ee02439e94c78721019d8d79
Kimberly Harrod <kim@fios.com>
2017-05-12 15:58:08 -0500

    Burger model

---
+++ models/burger.js
@@ -0,0 +1,22 @@
+
+// Sequelize (capital) references the standard library
+var Sequelize = require("sequelize");
+// sequelize (lowercase) references our connection to the database
+var sequelize = require("../config/connection.js");
+
+// Creates a "Burger" model that matches up with database
+var Burger = sequelize.define("burgers", {
+  // the burger name gets saved as a string
+  burger_name: Sequelize.STRING,
+  // whether or not the burger has been eaten is a Boolean
+  devoured: {
+      type: Sequelize.BOOLEAN,
+      defaultValue: false
+  }
+});
+
+// Syncs with database
+Burger.sync();
+
+// Makes the Burger Model available for other files and creates a table
+module.exports = Burger;
