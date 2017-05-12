bc54f453d79cdd0be7582f3bcea5280ab3225df7
Kimberly Harrod <kim@fios.com>
2017-05-12 15:51:30 -0500

    Dependencies and server

---
+++ server.js
@@ -0,0 +1,33 @@
+// Dependencies
+var express = require("express");
+var bodyParser = require("body-parser");
+var exphbs = require("express-handlebars");
+
+
+// Sets up the Express App
+var app = express();
+var PORT = process.env.PORT || 8088;
+
+// Sets up Handlebars
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+// Sets up the Express app to handle data parsing
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.text());
+app.use(bodyParser.json({ type: "application/vnd.api+json" }));
+
+// Static directory
+app.use(express.static("public/assets"));
+
+// Import routes and give the server access to them
+var routes = require("./controllers/burgers_controllers.js");
+
+app.use("/", routes);
+
+
+// Starts the server to begin listening
+app.listen(PORT, function() {
+  console.log("App listening on PORT " + PORT);
+});
